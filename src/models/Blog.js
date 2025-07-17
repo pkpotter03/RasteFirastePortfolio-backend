9d2ea1eee5053a75128caa1e3c9bc7c0e0f8b45c
pkpotter03 <pkpotter03@gmail.com>
2025-07-17 18:40:40 +0530

    update blogSchema

--- src/models/Blog.js
+++ src/models/Blog.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
   subtitle: { type: String },
+  placeNearBy: { type: String },
   typeOfLocation: { type: String },
-  difficultyLevel: { type: String },
   images: [{ type: String, required: true }], // 5 image links
   content: { type: String },
   locationLink: { type: String },
-  locationInfo: { type: String },
-  videoLink: { type: String },
+  igVideoLink: { type: String },
+  ytVideoLink: { type: String },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Blog', blogSchema); 
